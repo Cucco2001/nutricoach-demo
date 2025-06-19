f08415dac16724144b611131d4186d578d28e77b
Cucco2001 <giancarlo.cuccorese@gmail.com>
2025-06-19 10:57:26 +0200

    Aggiunto Caffè

--- agent_tools/meal_optimization_tool.py
+++ agent_tools/meal_optimization_tool.py
@@ -284,6 +284,7 @@ def get_portion_constraints():
     """
     return {
         # CATEGORIE PRINCIPALI DALLA BANCA ALIMENTI
+        "caffè": {"min": 5, "max": 5},
         "proteine_animali": {"min": 70, "max": 300},
         "latte": {"min": 100, "max": 500},
         "affettati": {"min": 50, "max": 150},
